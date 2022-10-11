fad233fb554709f855ea2867e672a7639ac97dfb
tuliorr <tulio.rodrigues.r@hotmail.com>
2022-10-11 16:02:40 -0300

    update main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # python -B main.py 32 0.75 42
 import sys
 import numpy as np
+from fbm1d import FBM1d
 from fbm2d import FBM2d
 
 L = int(sys.argv[1])
@@ -8,6 +9,8 @@ H = float(sys.argv[2])
 seed = int(sys.argv[3])
 
 np.random.seed(seed)
-arr = FBM2d(L, H)
+arr1d = FBM1d(L, H)
+arr2d = FBM2d(L, H)
 
-print(arr)
+print(arr1d)
+print(arr2d)
